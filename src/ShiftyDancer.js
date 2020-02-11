e59cacc1ffb48bc1efa34d7585cc0ba28281e5bb
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-11 10:22:53 -0800

    implement ShiftyDancer

---
+++ src/ShiftyDancer.js
@@ -0,0 +1,17 @@
+var ShiftyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass("shifty"); //add class
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+ShiftyDancer.prototype = Object.create(Dancer.prototype);
+
+ShiftyDancer.prototype.oldStep = Dancer.prototype.step;  
+
+ShiftyDancer.prototype.step = function () {
+
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  this.$node.toggle();
+};
