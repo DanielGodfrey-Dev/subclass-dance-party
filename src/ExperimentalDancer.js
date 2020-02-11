b88126e7ce6002d8cf6783357ae27a13ffc1b518
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-11 12:22:30 -0800

    add gideon and jace/chandra images with random selection

--- src/ExperimentalDancer.js
+++ src/ExperimentalDancer.js
@@ -1,6 +1,11 @@
 var ExperimentalDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass("experimental");
+  this.$node.prepend('<img src="./img/gideon.png" width="100"/>'); //gideon is dancing with
+  var choose = Math.floor(Math.random() * 2) % 2;                 //either
+  if (choose) {this.$node.append('<img src="./img/jace.png" width="100"/>');} //jace
+  else {this.$node.append('<img src="./img/chandra.png" width="100"/>');} //or chandra
+
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 };
