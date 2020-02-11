954ab1ebee87680fc62865dc89efc7dc082a991f
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-11 10:24:21 -0800

    update comment spacing

--- src/init.js
+++ src/init.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     }
   }*/
 
+
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
@@ -17,12 +18,10 @@ $(document).ready(function() {
      * maker functions available in the global scope, clicking that node
      * will call the function to make the dancer.
      */
-
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
+    /*dancerMakerFunctionName is a string which must match
+    * one of the dancer maker functions available in global scope.
+    * A new object of the given type will be created and added
+    * to the stage.*/     
     var dancerMakerFunctionName = $('.addDancerButton').data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
