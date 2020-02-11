63056092a2964bef10fc9582f1121d8073f7c7d1
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-10 20:23:44 -0800

    fix BlinkyDancer button

--- src/init.js
+++ src/init.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   window.dancers = [];
+  /*for (var key in window) {
+    var currentItem = window[key];
+    if (typeof currentItem === 'function') {
+      if (currentItem.prototype.constructor === Dancer) {
+        window.dancers.push(currentItem);
+      }
+    }
+  }*/
 
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
@@ -15,14 +23,14 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerMakerFunctionName = $('.addDancerButton').data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
