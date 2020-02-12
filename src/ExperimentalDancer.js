62c2847eb6c44edd07648b34054572de4ab8a32d
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-11 16:37:35 -0800

    turn off blink for ExperimentalDancer

--- src/ExperimentalDancer.js
+++ src/ExperimentalDancer.js
@@ -18,5 +18,5 @@ ExperimentalDancer.prototype.step = function () {
 
   // call the old version of step at the beginning of any call to this new version of step
   this.oldStep();
-  this.$node.toggle();
+  // this.$node.toggle();
 };
